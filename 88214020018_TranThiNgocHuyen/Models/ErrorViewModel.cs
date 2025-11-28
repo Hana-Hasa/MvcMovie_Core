5dfa5e806fbbfb82ed4b7367619bafebbbd34000
Huyen Tran <hanatran0897@gmail.com>
2025-11-28 17:55:52 +0700

    init app

---
+++ 88214020018_TranThiNgocHuyen/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace _88214020018_TranThiNgocHuyen.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
